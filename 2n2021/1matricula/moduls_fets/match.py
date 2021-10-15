bb99ef029de80920b5665735d249fcee3ea1ac91
Jorextror <joliveda@gmail.com>
2021-10-15 10:43:13 +0200

    007

--- 2n2021/1matricula/moduls_fets/match.py
+++ 2n2021/1matricula/moduls_fets/match.py
@@ -1,7 +1,6 @@
 """Es el m`odul que s’encarrega de determinar a quina xifra representa una imatge concreta."""
-from typing import ForwardRef
-from PIL import Image
-from img import subimg
+# from PIL import Image
+# from img import subimg
 import os
 
 def load_patterns(prefix):
@@ -10,9 +9,21 @@ def load_patterns(prefix):
     Per exemple, si l’argument  ́es patro voldr`a dir que els arxius dels patrons que s’hauran de
     llegir s’anomenaran: patro_0.jpeg, patro_1.jpeg, . . . , patro_9.jpeg."""
 
-    contenido = os.listdir('2n2021/1matricula/patrons')
-    print(contenido)
-
+    path='2n2021/1matricula/patrons'
+    content = os.listdir(path)
+    for i in range(len(content)):
+        archiu=content[i].split("_")
+        archiu[0]=prefix
+        os.rename(path+"/"+content[i],path+"/"+"_".join(archiu))
+    for i in range(len(content)-1):
+        if content[i].split("_")[1]=="gruixut":
+            try:
+                if content[i].split("_")[2]>content[i+1].split("_")[2]:
+                    content[i],content[i+1]=content[i+1],content[i]
+            except:
+                pass
+    return content
+print(load_patterns(prefix="patro"))
 # def match(img,patlst):
     
 load_patterns(prefix=2)
