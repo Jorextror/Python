5c3b685c5e7e05e87b10d5db8d140a8ce760e3b9
Jorextror <joliveda@gmail.com>
2021-10-17 21:48:25 +0200

    010 matricula pocho

--- 2n2021/1matricula/moduls_fets/match.py
+++ 2n2021/1matricula/moduls_fets/match.py
@@ -1,14 +1,11 @@
 """Es el m`odul que s’encarrega de determinar a quina xifra representa una imatge concreta."""
-# from PIL import Image
-# from img import subimg
-from img import subimg
+import img
 import os
-from imgio import read_bn,show,read_rgb
-from transf import scale,vtrim
-from discret import rgb_to_bn
+import imgio
+import transf
+import discret
+
 def load_patterns(prefix="patro"):
-    def ordena(elem):
-        return elem.split("_")[2][0]
     """Aquesta funci ́o rep com a par`ametre el prefix dels noms dels fitxers que contenen els patrons
     dels d ́ıgits i retorna la llista d’imatges corresponent als patrons dels d ́ıgits ordenats de 0 a 9.
     Per exemple, si l’argument  ́es patro voldr`a dir que els arxius dels patrons que s’hauran de
@@ -30,7 +27,13 @@ def load_patterns(prefix="patro"):
                 pass
     return content
         os.rename(path+"/"+content[i],path+"/"+"_".join(archiu))
-    content.sort(key=ordena)
+     for i in range(len(content)-1):
+            if content[i].split("_")[1]=="gruixut":
+            try:
+                if content[i].split("_")[2]>content[i+1].split("_")[2]:
+                    content[i],content[i+1]=content[i+1],content[i]
+            except:
+                pass
     return content
 
 def match(img,patlst):
@@ -39,38 +42,34 @@ def match(img,patlst):
     """Donada una llista de patrons patlst i una imatge img retorna un enter que correspon amb el
     dıgit mes semblant d’acord amb els conjunt de patrons usat. La imatge img ha de tenir la
     mateixa alçada que els patrons."""
-    similituds=[]
+    PixelIgual=[]
     k=0
-    for z in range(len(patlst)):
+    for l in range(len(patlst)):
         pi=0
-        comparation=read_bn("2 n/Tasca4.1/patrons/"+patlst[z])
-        #show(comparation)
+        patrons=read_bn("2 n/Tasca4.1/patrons/"+patlst[l])
         img=vtrim(img)
-        if len(comparation[1])<len(img[1]):
-            img=scale(img,len(comparation[1]))
+        if len(patrons[1])<len(img[1]):
+            img=scale(img,len(patrons[1]))
         else:
-            comparation=scale(comparation,len(img[1]))
-        if len(comparation[1][0])>len(img[1][0]):
-            columnes=len(comparation[1])
-            files=len(comparation[1][0])
-            alterFiles=len(img[1][0])
-            longer=comparation[1]
+            patrons=scale(patrons,len(img[1]))
+        if len(patrons[1][0])>len(img[1][0]):
+            column=len(patrons[1])
+            row=len(patrons[1][0])
+            RowImg=len(img[1][0])
+            longer=patrons[1]
             shorter=img[1]
         else:
-            columnes=len(img[1])
-            files=len(img[1][0])
-            alterFiles=len(comparation[1][0])
+            column=len(img[1])
+            row=len(img[1][0])
+            RowImg=len(patrons[1][0])
             longer=img[1]
-            shorter=comparation[1]
-        for i in range(files):
-            for k in range(i,i+files-alterFiles+1):
-                for j in range(columnes):
+            shorter=patrons[1]
+        for i in range(row):
+            for k in range(i,i+row-RowImg+1):
+                for j in range(column):
                     if longer[j][k]==shorter[j][k]:
                         pi+=1
-
-        similituds.append((patlst[z].split("_")[len(patlst[z].split("_"))-1][0],pi))
-    similituds.sort(key=ordena,reverse=True)
-    print(similituds)
-    return similituds[0][0]
-
-print(match(read_bn("2 n\Tasca4.1\sortida\digit_3.jpeg"),load_patterns()))
+        PixelIgual.append((patlst[l].split("_")[len(patlst[l].split("_"))-1][0],pi))
+    PixelIgual.sort(key=ordena,reverse=True)
+    print(PixelIgual)
+    return PixelIgual[0][0]
