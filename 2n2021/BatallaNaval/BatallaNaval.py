15f25d0d495726ffe847a408c4101e4e012970cf
Jorextror <joliveda@gmail.com>
2021-11-19 20:28:55 +0100

    020

--- 2n2021/BatallaNaval/BatallaNaval.py
+++ 2n2021/BatallaNaval/BatallaNaval.py
@@ -1,7 +1,8 @@
-"""
+"""Joel Farell i Jordi Oliveda
+
 Fes un joc de batalla naval per jugar contra l'ordinador.
 Especificacions:
-
+sim
 Taulell de 10x10 posicions.
 Hi col·locarem aleatòriament els següents vaixells ( no es poden tocar entre ells):
 1 portaavions de 4 caselles.
