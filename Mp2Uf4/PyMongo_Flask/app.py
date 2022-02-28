ea333364aea9a6a2f49c570e9493e829a64ba28d
Jorextror <joliveda@gmail.com>
2022-02-28 20:57:19 +0100

    018

--- Mp2Uf4/PyMongo_Flask/app.py
+++ Mp2Uf4/PyMongo_Flask/app.py
@@ -1,24 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect
 from pymongo import MongoClient, collection
 import requests
 
-
 app = Flask(__name__)
 @app.route('/')
-@app.route('/<name>')
+# @app.route('/<name>')
 def hello(name=None):
 	client=MongoClient()
 	with MongoClient('localhost',27017,username='Jordi',password='Admin@123',authSource='JordiOlivedaBD') as client:
 		print("Conectado a MongoDB") #Connectar con el servidor de MongoDB
 		db= client.JordiOlivedaBD #database
-		col=db["tasca4"] #crear collection
+		col=db["Usuaris"] #crear collection
 		return render_template('index.html',lista=col.find())
 
-@app.route('/templates/index.html', methods=['GET', 'POST'])
+@app.route('/index.html', methods=['GET', 'POST'])
 def validarLogin():
 	user=requests.post(user)
 
-
-@app.route('/templates/registrar.html')
-def validarRegistrar():
-	pass
+@app.route('/registrar')
+def registrar():
+	return render_template('registrar.html')
