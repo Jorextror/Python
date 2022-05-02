705ef968190f63500df3ef962c7fa7fe673b2cc8
Jorextror <joliveda@gmail.com>
2022-05-02 15:16:22 +0200

    038-django

--- 2n2021/POO-PersistenciaBD-Django/mysite/polls/models.py
+++ 2n2021/POO-PersistenciaBD-Django/mysite/polls/models.py
@@ -1,10 +1,11 @@
-from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 import datetime
+# Username: admin
+# Email address: admin@example.com
+from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-@python_2_unicode_compatible # only if you need to support Python 2
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -12,7 +13,7 @@ class Question(models.Model):
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-@python_2_unicode_compatible # only if you need to support Python 2
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
